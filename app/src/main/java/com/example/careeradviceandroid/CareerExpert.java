c77fbb858605eadbd225ac548df10d32dd5d4ef7
AruSahu <aryansahu@arusmacbook.mynetworksettings.com>
2024-01-21 19:49:20 -0700

    first commit

---
+++ app/src/main/java/com/example/careeradviceandroid/CareerExpert.java
@@ -0,0 +1,19 @@
+package com.example.careeradviceandroid;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CareerExpert {
+    List<String> getStreams(String course) {
+        List<String> stream = new ArrayList<String>();
+        if (course.equals("Bio")) {
+            stream.add("MBBS");
+            stream.add("B.Sc Biologist");
+        } else {
+            stream.add("B.Tech");
+            stream.add("B.Sc. PCM");
+        }
+        return stream;
+    }
+}
